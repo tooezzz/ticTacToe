42a03b042a3c9aee858396c61d010e1e8262f3d6
mixa <!@#$%^&*()>
2020-04-27 10:14:43 +0800

    Done

---
+++ Big_figures/Big_figures.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+
+const int W = 19, H = 11;
+char field[W][H];
+
+using namespace std;
+
+void draw_x(int i, int j);
+
+void draw_x(int i, int j) {
+    int ii = i * 4;
+    int jj = j * 7;
+
+    field[ii][jj] = 'x';
+    field[ii + 1][jj + 2] = 'x';
+    field[ii + 2][jj + 4] = 'x';
+    field[ii][jj + 4] = 'x';
+    field[ii + 2][jj] = 'x';
+}
+
+int main()
+{
+    
+
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            field[i][j] = '-';
+        }
+    }
+    
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            cout << field[i][j];
+        }
+
+        cout << endl;
+    }
+
+    system("cls");
+
+    int iMove = 0, jMove = 0;
+    int counter = 0;
+    do {
+
+        /*cout << "type: ";
+        cin >> iMove >> jMove;*/
+
+        draw_x(0, 0);
+        draw_x(0, 1);
+        system("cls");
+        draw_x(1, 0);
+               
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                cout << field[i][j];
+            }
+            cout << endl;
+        }
+
+        counter++;
+    } while (counter != 3);
+
+    system("pause");
+    return 0;
+}
--- Tic_Tac_Toe/Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe/Tic_Tac_Toe.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <ctime>
+#include <math.h>
 
 using namespace std;
 
@@ -13,7 +14,8 @@ void startField();
 void playerMove();
 void pcMove();
 void showField();
-char winCheck();
+char winCheck(char check);
+bool drawCheck();
 
 //Пустое поле
 void startField() {
@@ -39,11 +41,19 @@ void showField() {
 
 //Ход игрока
 void playerMove() {
-    int i;
-    int j;
+    int i = 1;
+    int j = 1;
+     
     cout << "Your turn: ";
-    cin >> i >> j;  
-    gameField[i - 1][j - 1] = 'x'; //-1 для ввода "1 1", "2 2" и т.д.
+    cin >> i >> j;
+
+    if (gameField[i - 1][j - 1] == '*') {        
+        gameField[i - 1][j - 1] = 'x'; //-1 для ввода "1 1", "2 2" и т.д.
+    }
+    else if (gameField[i - 1][j - 1] != '*') {        //Если ячейка не пустая (не равна *), то ход повторить
+        cout << "Not empty slot! Repeat your turn: ";
+        playerMove();        
+    }
 }
 
 //Генерация хода ПК
@@ -51,77 +61,145 @@ void pcMove() {
     int i = 0;
     int j = 0;
 
-    for (int q = 0; q < fieldSize * 2; ++q) {
-        
+    for (;;) {
+
+        //Рандомные значения для хода ПК
+        i = 0 + rand() % fieldSize;
+        j = 0 + rand() % fieldSize;
+
         //Проверка на пустую ячейку
-        if (gameField[i][j] == '*' && gameField[i][j] != 'x' && gameField[i][j] != 'o') {
-            i = 0 + rand() % 3;
-            j = 0 + rand() % 3;
+        if (gameField[i][j] == '*') {
             gameField[i][j] = 'o';
             break;
         }
-        i++;
-        j++;
+        else if (drawCheck() == true) { //Если пустых клеток нет, то объявление ничьей и выход из цикла
+            cout << "Draw" << endl;
+            break;
+        }
     }
 }
 
-//Проверка на победу
-char winCheck(){
-    int i;
-    char* check;
-    for (i = 0; i < fieldSize; i++) {
-        check = &gameField[i][0];
-        if (*check == *(check + 1) && *(check + 1) == *(check + 2))
-            return *check;
+//Проверка на ничью
+bool drawCheck() {
+    int i = 0;
+    int j = 0;
+    char check = '*';
+    int counter = 0;
+
+    for (i; i < pow(fieldSize, 2); i++) {       //Проверяются все ячейки
+        for (j; j < pow(fieldSize, 2); j++) {
+            if (gameField[i][j] == check)       //Если в поле есть *, то counter + 1
+                counter++;
+        }
     }
 
+    if (counter == 0) return true;              //Если counter = 0, значит, на поле нет пустых клеток
+
+    return false;
+}
+
+//Проверка на победу
+char winCheck(char check){
+    int i, j;
+    bool result = true; //Переменная для проверки истиности заполнения линий
+
+    //Проверка строк
     for (i = 0; i < fieldSize; i++) {
-        check = &gameField[0][i];
-        if (*check == *(check + 3) && *(check + 3) == *(check + 6))
-            return *check;
+        result = true;
+
+        for (j = 0; j < fieldSize; j++) {
+            if (gameField[i][j] != check) {
+                result = false;
+                break;
+            }
+        }
+        if (result) return true;
     }
+
+    //Проверка столбцов
+    for (j = 0; j < fieldSize; ++j) {
+        result = true;
+        for (i = 0; i < fieldSize; ++i) {
+            if (gameField[i][j] != check) {
+                result = false;
+                break;
+            }
+        }
+        if (result) return true;
+    }
+
+    //Проверка первой диагонали
+    for (i = 0; i < fieldSize; ++i) {
+        result = true;
+
+        if (gameField[i][i] != check) {
+            result = false;
+            break;
+        }
+    }
+    if (result) return true;
+
+    //Проверка второй диагонали
+    for (i = 0; i < fieldSize; ++i) {
+        result = true;
+
+        if (gameField[i][fieldSize - i - 1] != check) {
+            result = false;
+            break;
+        }
+    }
+    if (result) return true;
+
+    return false;
 }
 
 int main()
 {
-    char result = '*';
-
     srand(time(NULL));
+
+    char result = '*';    
+    bool win = true;
+    bool draw = true;
 
     //Стартовое поле
     startField();
 
     //Ход игры
     do {
+        //Проверка на ничью
+        if (draw == drawCheck())
+            break;
+
         //Ход игрока
         playerMove();
         system("cls");
         //Проверка на победу
-        result = winCheck();
-        if (result != '*' && result != 'o')
+        if (win == winCheck('x')) {
+            result = 'x';
             break;
+        }
 
         //Генерация хода ПК
         pcMove();
         //Проверка на победу
-        result = winCheck();
-        if (result != '*' && result != 'x')
+        if (win == winCheck('o')) {
+            result = 'o';
             break;
+        }
 
         //Вывод массива
         showField();
-    } while (result == '*');
+    } while (1);
 
-    if (result == 'o') {
-        system("cls");
-        cout << "PC won" << endl;
-    }
-    else {
-        system("cls");
-        cout << "Player won" << endl;
-    }
-
+    system("cls");
     showField();
+
+    if (result == 'o')        
+        cout << "PC won" << endl;
+    else if (drawCheck() == true)        
+        cout << "Draw" << endl;
+    else        
+        cout << "Player won" << endl;
 
     system("pause");
     return 0;
